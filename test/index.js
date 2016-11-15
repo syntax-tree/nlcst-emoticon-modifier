3b0973035088258cc8d758e4f81ccabdac1a865f
Titus Wormer <tituswormer@gmail.com>
2016-11-15 20:49:49 +0100

    Replace mocha with tape

--- test/index.js
+++ test/index.js
@@ -1,82 +1,67 @@
 'use strict';
 
-/* eslint-env mocha */
-
+var fs = require('fs');
+var path = require('path');
 var assert = require('assert');
+var test = require('tape');
 var retext = require('retext');
 var english = require('retext-english');
 var emoticons = require('emoticon');
+var negate = require('negate');
+var hidden = require('is-hidden');
+var toString = require('nlcst-to-string');
 var modifier = require('..');
 
-var smile = require('./fixtures/smile');
-var invalid = require('./fixtures/invalid');
-var adjacent = require('./fixtures/adjacent');
-var sentenceStart = require('./fixtures/sentence-start');
-
-/* Processors. */
 var position = retext(english).use(plugin);
 var noPosition = retext(english).use(plugin).use(function (instance) {
   instance.Parser.prototype.position = false;
 });
 
-/*
- * Tests.
- */
+test('nlcst-emoticon-modifier()', function (t) {
+  var root = path.join(__dirname, 'fixtures');
 
-describe('nlcst-emoticon-modifier()', function () {
-  it('should throw when not given a parent', function () {
-    assert.throws(
-      function () {
-        modifier({});
-      },
-      /Missing children in `parent`/
-    );
-  });
+  t.throws(
+    function () {
+      modifier({});
+    },
+    /Missing children in `parent`/,
+    'should throw when not given a parent'
+  );
 
-  it('should classify emoticons (`:)`)', function () {
-    check('This makes me feel :).', smile);
-  });
+  fs
+    .readdirSync(root)
+    .filter(negate(hidden))
+    .forEach(function (filename) {
+      var tree = JSON.parse(fs.readFileSync(path.join(root, filename)));
+      var fixture = toString(tree);
+      var name = path.basename(filename, path.extname(filename));
 
-  it('should NOT classify invalid emoticons (`):` and `: (`)', function () {
-    check('Well: (This makes me feel):.', invalid);
-  });
+      t.deepEqual(position.parse(fixture), tree, name);
+      t.deepEqual(noPosition.parse(fixture), clean(tree), name + ' (positionless)');
+    });
 
-  it('should classify adjacent emoticons (`:) :(`)', function () {
-    check('This makes me feel :) :(.', adjacent);
-  });
-
-  it('should classify emoticons at sentence start', function () {
-    check('<3 I’m going to bed.', sentenceStart);
-  });
+  t.end();
 });
 
-/*
- * Check all emoticons.
- */
+test('emoticons', function (t) {
+  emoticons.forEach(function (emoticon) {
+    emoticon.emoticons.forEach(function (value) {
+      var fixture = 'Who doesn’t like ' + value + '?';
+      var node = position.run(position.parse(fixture));
+      var emoticon = node.children[0].children[0].children[6];
 
-emoticons.forEach(function (emoticon) {
-  emoticon.emoticons.forEach(function (value) {
-    describe(value, function () {
-      it(
-        'should classify `' + value + '` as an `EmoticonNode`',
+      t.doesNotThrow(
         function () {
-          var fixture = 'Who doesn’t like ' + value + '?';
-          var node = position.run(position.parse(fixture));
-          var emoticon = node.children[0].children[0].children[6];
-
           assert.strictEqual(emoticon.type, 'EmoticonNode');
           assert.strictEqual(emoticon.value, value);
-        }
+        },
+        value
       );
     });
   });
-});
 
-/* Short-cut to access the CST. */
-function check(fixture, node) {
-  assert.deepEqual(position.run(position.parse(fixture)), node);
-  assert.deepEqual(noPosition.run(noPosition.parse(fixture, {position: false})), clean(node));
-}
+  t.end();
+});
 
 /* Add modifier to processor. */
 function plugin(processor) {
