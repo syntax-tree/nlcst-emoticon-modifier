5c8e29704222919d99380775f5a6793b990e77fb
Titus Wormer <tituswormer@gmail.com>
2020-11-02 16:47:06 +0100

    Refactor to improve bundle size

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ var raw = require('emoticon')
 
 module.exports = modifier(mergeEmoticons)
 
-var emoticonNode = 'EmoticonNode'
-
 // Magic numbers.
 //
 // Emoticons are treated by a parser as multiple nodes.
@@ -28,37 +26,32 @@ unpack()
 
 // Merge emoticons into an `EmoticonNode`.
 function mergeEmoticons(child, index, parent) {
-  var siblings
   var value
   var siblingIndex
   var node
   var emoticon
-  var subvalue
 
   // Check if `child`s first character could be used to start an emoticon.
   if (start.indexOf(toString(child).charAt(0)) !== -1) {
-    siblings = parent.children
+    value = ''
     siblingIndex = index
     node = child
-    value = ''
 
     while (node) {
       if (value.length >= maxEmoticonLength) {
         return
       }
 
-      subvalue = toString(node)
-
-      value += subvalue
+      value += toString(node)
 
       // The second test, if the last character of the current node is
       // superfluous but improves performance by 30%.
       if (
-        node.type !== emoticonNode &&
-        end.indexOf(subvalue.charAt(subvalue.length - 1)) !== -1 &&
+        node.type !== 'EmoticonNode' &&
+        end.indexOf(value.charAt(value.length - 1)) !== -1 &&
         emoticons.indexOf(value) !== -1
       ) {
-        emoticon = {type: emoticonNode, value: value}
+        emoticon = {type: 'EmoticonNode', value: value}
 
         if (child.position && node.position) {
           emoticon.position = {
@@ -67,46 +60,36 @@ function mergeEmoticons(child, index, parent) {
           }
         }
 
-        siblings.splice(index, siblingIndex - index + 1, emoticon)
+        parent.children.splice(index, siblingIndex - index + 1, emoticon)
 
         // Some emoticons, like `:-`, can be followed by more characters to form
         // other emoticons.
         return index - 1
       }
 
-      node = siblings[++siblingIndex]
+      node = parent.children[++siblingIndex]
     }
   }
 }
 
 function unpack() {
-  var length = raw.length
   var index = -1
   var subset
   var offset
-  var count
-  var subvalue
   var char
 
-  while (++index < length) {
+  while (++index < raw.length) {
     subset = raw[index].emoticons
-    count = subset.length
     offset = -1
 
-    while (++offset < count) {
-      subvalue = subset[offset]
+    while (++offset < subset.length) {
+      emoticons.push(subset[offset])
 
-      emoticons.push(subvalue)
+      char = subset[offset].charAt(0)
+      if (start.indexOf(char) === -1) start.push(char)
 
-      char = subvalue.charAt(0)
-      if (start.indexOf(char) === -1) {
-        start.push(char)
-      }
-
-      char = subvalue.charAt(subvalue.length - 1)
-      if (end.indexOf(char) === -1) {
-        end.push(char)
-      }
+      char = subset[offset].charAt(subset[offset].length - 1)
+      if (end.indexOf(char) === -1) end.push(char)
     }
   }
 }
